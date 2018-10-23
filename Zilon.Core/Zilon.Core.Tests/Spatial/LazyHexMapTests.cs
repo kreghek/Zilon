734d4ed3d81a14564ddb41411197c61327edab6c
Pavel Kurkutov <kurkutovpa@gmail.com>
2018-10-23 23:37:07 +0700

    Добавлен тест на глобальную карту и настройки инспектора решарпер.

---
+++ Zilon.Core/Zilon.Core.Tests/Spatial/LazyHexMapTests.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+using FluentAssertions;
+
+using NUnit.Framework;
+
+using Zilon.Core.Spatial;
+
+namespace Zilon.Core.Tests.Spatial
+{
+    [TestFixture]
+    public class LazyHexMapTests
+    {
+        [Test]
+        public void LazyHexMapTest()
+        {
+            // ARRANGE
+            const int segmentSize = 100;
+
+
+
+            // ACT
+            Action act = () =>
+            {
+                new LazyHexMap(segmentSize);
+            };
+
+
+            // ASSERT
+            act.Should().NotThrow();
+        }
+    }
+}
