ed7582b0b2f87d9702b4eba503161f4cf1230dc4
Pavel Kurkutov <kurkutovpa@gmail.com>
2018-10-31 21:13:31 +0700

    Исправлен тест на сервис боя.

--- Zilon.Core/Zilon.Core.Tests/Combat/CombatServiceTests.cs
+++ Zilon.Core/Zilon.Core.Tests/Combat/CombatServiceTests.cs
@@ -14,12 +14,15 @@ namespace Zilon.Core.Tests.Combat
         public void UseSkillTest()
         {
             var diceMock = new Mock<IDice>();
+            diceMock.Setup(x => x.Roll(It.IsAny<int>())).Returns<int>(n => n / 2);
             var dice = diceMock.Object;
 
             var attackerSquadMock = new Mock<ICombatSquad>();
+            AddPersonsToSquadMock(attackerSquadMock);
             var attackerSquad = attackerSquadMock.Object;
 
             var targetSquadMock = new Mock<ICombatSquad>();
+            AddPersonsToSquadMock(targetSquadMock);
             var targetSquad = targetSquadMock.Object;
 
             var combatService = new CombatService(dice);
@@ -33,6 +36,25 @@ namespace Zilon.Core.Tests.Combat
 
             // ASSERT
             combatEvents.ElementAt(0).Should().BeOfType<AttackCombatEvent>();
+        }
+
+        private void AddPersonsToSquadMock(Mock<ICombatSquad> squadMock)
+        {
+            squadMock.SetupGet(x => x.Persons).Returns(new[]
+            {
+                CreatePerson(),
+                CreatePerson(),
+                CreatePerson(),
+                CreatePerson(),
+                CreatePerson()
+            });
+        }
+
+        private ICombatPerson CreatePerson()
+        {
+            var personMock = new Mock<ICombatPerson>();
+            personMock.SetupGet(x => x.HitPoints).Returns(10);
+            return personMock.Object;
         }
     }
 }
